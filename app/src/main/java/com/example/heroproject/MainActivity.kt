f6b39b20725da9f1bb5fe0f15f5948f0f0729d6a
Aslan <83723440+dexsid@users.noreply.github.com>
2023-07-25 14:07:45 +0600

    Начал изучать ресурсы и вставил картинки в свой проект

--- app/src/main/java/com/example/heroproject/MainActivity.kt
+++ app/src/main/java/com/example/heroproject/MainActivity.kt
@@ -17,40 +17,55 @@ class MainActivity : AppCompatActivity() {
         bClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bClass.root)
         bClass.btResult.setOnClickListener {
+
             val result = bClass.edValue.text.toString()
             bClass.tvAnswer.visibility = View.VISIBLE
+            bClass.imPhoto.visibility = View.VISIBLE
             val tempText = ""
 
             when (result) {
 
                 Constance.DVORNIK -> {
                     val temptext = "На вашу карту начисленно ${Constance.DVORNIK_SUELDO}"
-                    bClass.tvAnswer.text = if (bClass.edValue2.text.toString() == Constance.DVORNIK_PASSWORD) {
-                        temptext
+                    if (bClass.edValue2.text.toString() == Constance.DVORNIK_PASSWORD) {
+                        bClass.tvAnswer.text = temptext
+                        bClass.imPhoto.setImageResource(R.drawable.photo)
                     }else {
-                        "Не верный пароль"
+                        bClass.tvAnswer.text = "Не верный пароль"
+                        bClass.imPhoto.visibility = View.GONE
+
                     }
 
                 }
                 Constance.BOSS -> {
                     val temptext = "На вашу карту начисленно ${Constance.BOSS_SUELDO}"
-                    bClass.tvAnswer.text = if (bClass.edValue2.text.toString() == Constance.BOSS_PASSWORD) {
-                        temptext
+                    if (bClass.edValue2.text.toString() == Constance.BOSS_PASSWORD) {
+                        bClass.tvAnswer.text = temptext
+                        bClass.imPhoto.setImageResource(R.drawable.photo)
+
                     }else {
-                        "Не верный пароль"
+                        bClass.tvAnswer.text = "Не верный пароль"
+                        bClass.imPhoto.visibility = View.GONE
+
                     }
 
                 }
                 Constance.MEGABOSS -> {
                     val temptext = "На вашу карту начисленно ${Constance.MEGABOSS_SUELDO}"
-                    bClass.tvAnswer.text = if (bClass.edValue2.text.toString() == Constance.MEGABOSS_PASSWORD) {
-                        temptext
+                    if (bClass.edValue2.text.toString() == Constance.MEGABOSS_PASSWORD) {
+                        bClass.tvAnswer.text = temptext
+                        bClass.imPhoto.setImageResource(R.drawable.photo)
+
                     }else {
-                        "Не верный пароль"
+                        bClass.tvAnswer.text = "Не верный пароль"
+                        bClass.imPhoto.visibility = View.GONE
+
                     }
                 }
                 else -> {
                     bClass.tvAnswer.text = "Вы не являетесь сотрудником"
+                    bClass.imPhoto.visibility = View.GONE
+
                 }
             }
 
