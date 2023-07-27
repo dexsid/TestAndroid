1cdbdb2064b59a3f97c218449bf91338cfc4511d
Aslan <83723440+dexsid@users.noreply.github.com>
2023-07-27 14:43:27 +0600

    Прошёл 15 урок, изучил разные состояния: перевод на разные языки, оринтация приложения, и панелька xml

--- app/src/main/java/com/example/heroproject/MainActivity.kt
+++ app/src/main/java/com/example/heroproject/MainActivity.kt
@@ -16,60 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(s)
         bClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bClass.root)
-        bClass.btResult.setOnClickListener {
 
-            val result = bClass.edValue.text.toString()
-            bClass.tvAnswer.visibility = View.VISIBLE
-            bClass.imPhoto.visibility = View.VISIBLE
-            val tempText = ""
-
-            when (result) {
-
-                Constance.DVORNIK -> {
-                    val temptext = "На вашу карту начисленно ${Constance.DVORNIK_SUELDO}"
-                    if (bClass.edValue2.text.toString() == Constance.DVORNIK_PASSWORD) {
-                        bClass.tvAnswer.text = temptext
-                        bClass.imPhoto.setImageResource(R.drawable.photo)
-                    }else {
-                        bClass.tvAnswer.text = "Не верный пароль"
-                        bClass.imPhoto.visibility = View.GONE
-
-                    }
-
-                }
-                Constance.BOSS -> {
-                    val temptext = "На вашу карту начисленно ${Constance.BOSS_SUELDO}"
-                    if (bClass.edValue2.text.toString() == Constance.BOSS_PASSWORD) {
-                        bClass.tvAnswer.text = temptext
-                        bClass.imPhoto.setImageResource(R.drawable.photo)
-
-                    }else {
-                        bClass.tvAnswer.text = "Не верный пароль"
-                        bClass.imPhoto.visibility = View.GONE
-
-                    }
-
-                }
-                Constance.MEGABOSS -> {
-                    val temptext = "На вашу карту начисленно ${Constance.MEGABOSS_SUELDO}"
-                    if (bClass.edValue2.text.toString() == Constance.MEGABOSS_PASSWORD) {
-                        bClass.tvAnswer.text = temptext
-                        bClass.imPhoto.setImageResource(R.drawable.photo)
-
-                    }else {
-                        bClass.tvAnswer.text = "Не верный пароль"
-                        bClass.imPhoto.visibility = View.GONE
-
-                    }
-                }
-                else -> {
-                    bClass.tvAnswer.text = "Вы не являетесь сотрудником"
-                    bClass.imPhoto.visibility = View.GONE
-
-                }
-            }
-
-        }
     }
 
 
